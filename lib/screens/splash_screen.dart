9549d0a1fc7b03b553f219d75a1df52ea8049b8a
Basit ali <basitalyshah51214@gmail.com>
2025-03-28 03:59:01 -0700

    Add Google Fonts dependency and implement SplashScreen with styled text

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
@@ -7,7 +8,12 @@ class SplashScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children: [],
+        children: [
+          Image.asset("images/splash_screen.dart"),
+          Text("Grocery Plus",
+              style: GoogleFonts.poppins(
+                  fontSize: 24, fontWeight: FontWeight.w500))
+        ],
       ),
     );
   }
